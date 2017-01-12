4b4043d27a86b136667db19d723debce7f680f53
Derek Pham <derekmpham@gmail.com>
2017-01-12 11:20:44 -0800

    Add page counter and replace jquery with other javascript

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
-$.get(chrome.extension.getURL("statusbar.html"), {}, function(data) {$('body').append(data);}, 'html');
+var xhReq = new XMLHttpRequest();
+xhReq.onreadystatechange = function onSumResponse() {
+	if (xhReq.readyState == 4) {
+		var serverResponse = xhReq.responseText;
+		var body = document.getElementsByTagName("body")[0];
+		var div = document.createElement('div');
+		div.innerHTML = serverResponse;
+		body.appendChild(div);
+	}
+}
+xhReq.open("GET", chrome.extension.getURL("statusbar.html"), true);
+xhReq.send(null);
+
 chrome.extension.sendRequest({}, function(response) {});
 
-$(document).ready(function(){
-	countWords();
-});
+var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		countWords();
+		clearInterval(readyStateCheckInterval);
+	}
+}, 10);
 
 function countWords() {
-	var number = 0;
-	$('span.kix-lineview-text-block').each(function(i, obj){
-		number += $(obj).text().split(/\s+/).length;
-	});
-	$('span#wordsTotal').text(number + 'total words');
+	var pageCount = 0;
+	var divs = document.getElementsByTagName('div'), i;
+	for (i in divs) {
+		if((" " + divs[i].className + " ").indexOf(" kix-page ") > -1) { pageCount++; }
+	}
+	var wordCount = 0;
+	var spans = document.getElementsByTagName('span'), i; 
+	for (i in spans) {
+		if((" " + spans[i].className + " ").indexOf(" kix-lineview-text-block ") > -1) {
+			var words = spans[i].innerText.replace(/W+/g, ' ').match(/S+/g);
+			wordCount += words &&
+			words.length || 0;
+		}
+	}
+	document.getElementById('wordsTotal').innerText = pageCount + ' pages, ' + wordCount + ' total words';
 	timeout = setTimeout('countWords()', 5000);
 }
